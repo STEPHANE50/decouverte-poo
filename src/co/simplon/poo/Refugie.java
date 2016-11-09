294e9770c6d08e9d67d830ab2acbd29e04fe14b1
Eric SIBER <eric.siber@gmail.com>
2016-11-09 01:18:43 +0100

    Step 5 sans modification sur Personne

---
+++ src/co/simplon/poo/Refugie.java
@@ -0,0 +1,72 @@
+package co.simplon.poo;
+
+import java.util.Date;
+
+public class Refugie extends Personne {
+
+	private boolean regularise;
+	private Date dateLimiteSejour;
+
+	public Refugie(String prenom, String nom, int age, Date dateLimiteSejour) {
+		super(prenom, nom, age);
+		this.dateLimiteSejour = dateLimiteSejour;
+		this.regularise = false;
+	}
+
+	public boolean demanderAsile(Adresse adresse) {
+		if (dateLimiteSejour.after(new Date())) {
+			this.regularise = true;
+			this.setAdresse(adresse);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void declineToIdentite() {
+		System.out.println("Je suis " + this.getPrenom() + " " + this.getNom());
+		if (this.regularise) {
+			System.out.println("Je suis un réfugié régularisé vivant à " + this.getAdresse().getVille());
+		} else {
+			System.out.println("Je suis en attente de régularisation.");
+		}
+	}
+	
+	@Override
+	public void demenage(Adresse adresse) {
+		if (this.regularise) {
+			if (adresse.getCodePostal() == this.getAdresse().getCodePostal()
+					&& adresse.getVille().equals(this.getAdresse().getVille())) {
+				super.demenage(adresse);
+			} else {
+				System.out
+						.println("Le réfugié "
+								+ this.getPrenom()
+								+ " "
+								+ this.getNom()
+								+ "ne peut déménager en dehors de sa ville de résidence actuelle");
+			}
+		} else {
+			System.out
+					.println("Le réfugié "
+							+ this.getPrenom()
+							+ " "
+							+ this.getNom()
+							+ " ne peut définir d'adresse tant qu'il n'est pas régularisé.");
+		}
+	}
+
+	@Override
+	public void setAdresse(Adresse adresse) {
+		throw new UnsupportedOperationException("Il est interdit de modifier l'adresse d'un réfugié");
+	}
+	
+	public boolean isRegularise() {
+		return regularise;
+	}
+
+	public Date getDateLimiteSejour() {
+		return dateLimiteSejour;
+	}
+
+}
